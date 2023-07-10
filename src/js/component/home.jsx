b6fa7ff68c0cfaaa363a61ed92d19c392c3c18c6
Javier Torres <127144225+JavierOTorres@users.noreply.github.com>
2023-07-10 22:37:56 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,39 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Jumbotron from "./jumbotron";
+import Navbar from "./navbar";
+import Card from "./card";
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <Navbar />
+      <div className="container-fluid px-5 my-3">
+        <Jumbotron />
+        <div className="row">
+          <div className="col">
+            <Card imgURL="https://i.pinimg.com/originals/d1/73/59/d173596bfc14577a6a6e923b2c25d71a.gif" />
+          </div>
+          <div className="col">
+            <Card />
+          </div>
+          <div className="col">
+            <Card cardText="This card does have some text. I guess you're not a goober" />
+          </div>
+          <div className="col">
+            <Card
+              imgURL="https://media.tenor.com/kQleiyX5Vb4AAAAC/oblivion-the-elder-scrolls.gif"
+              cardTitle="Farewell"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="bg-dark p-3 text-center">
+        <p className="text-light mb-0">Footer 2023</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
